4a2b712b1d2288db404100fe69ee2ba0843f18f0
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-07-12 08:32:56 -0700

    url

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+
 var http = require('http');
+var URL = require('url');
+
 
 var server = http.createServer(function(req,res){
+ var myOne = "http://rabbil.com/blog.html?year=2020&month=july"
 
-    res.end('hello word')
+ var myobjat =URL.parse(myOne.true);
+
+ var myF=myobjat.host;
+ var myB=myobjat.pathname;
+ var myc=myobjat.search;
+ write(myB)
+
+
+
 
 })
 
